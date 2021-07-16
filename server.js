737aa1baddfbe8ff539298b94d81a072e3b445dc
Imaad Uddin <imaaduddin@icloud.com>
2021-07-16 16:59:48 -0500

    database

--- server.js
+++ server.js
@@ -85,16 +85,16 @@ app.post("/register", (req, res) => {
 
 app.get("/profile/:id", (re, res) => {
   const {id} = req.params;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
-    } 
+  db.select("*").from("users").where({
+    id: id
   })
-  if (!found) {
-    res.status(400).json("status not found");
-  }
+    .then(user => {
+      if (user.length) {
+        res.json(user[0])
+      } else {
+        res.status(400).json("error getting user")
+      }
+  })
 })
 
 app.put("/image", (req, res) => {
